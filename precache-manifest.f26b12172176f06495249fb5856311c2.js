3240fbf968e597f06e5e6fd4da81f375754c9bca
zaid <khanzaid786786.kz@gmail.com>
2020-01-17 10:37:36 +0530

    Updates

---
+++ precache-manifest.f26b12172176f06495249fb5856311c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0183c392a517eb61caa32416707a3cbe",
+    "url": "/adidas_app/index.html"
+  },
+  {
+    "revision": "958989f0bb6221af24a3",
+    "url": "/adidas_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df62880d0b52f756465c",
+    "url": "/adidas_app/static/js/2.b6d87458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adidas_app/static/js/2.b6d87458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "958989f0bb6221af24a3",
+    "url": "/adidas_app/static/js/main.357a4176.chunk.js"
+  },
+  {
+    "revision": "66770043765b8fa336c7",
+    "url": "/adidas_app/static/js/runtime-main.7d7c06da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adidas_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
